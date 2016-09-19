5679e258770a294c9d12f143b2532deb9c333aa0
Slobodan Djordjevic <sdjordjevic@razor.rs>
2016-09-19 11:05:02 +0200

    Adding ngdocs

--- gulpfile.js
+++ gulpfile.js
@@ -28,9 +28,31 @@ gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
 
-gulp.task('ngdocs', [], function () {
+/**
+*   Generate documentation
+*/
+gulp.task('ngdocs_generate', [], function () {
   var gulpDocs = require('gulp-ngdocs');
-  return gulp.src('path/to/src/*.js')
+  return gulp.src('./src/app/**/*.js')
     .pipe(gulpDocs.process())
     .pipe(gulp.dest('./docs'));
+});
+
+
+/**
+*   Serve documentation
+*/
+gulp.task('connect_ngdocs', function() {
+var connect = require('gulp-connect');
+  connect.server({
+    root: 'docs',
+    livereload: false,
+    fallback: 'docs/index.html',
+    port: 8083
+  });
+});
+
+var runSequence = require('run-sequence');
+gulp.task('ngdocs', ['clean'], function () {
+    runSequence('ngdocs_generate', 'connect_ngdocs');
 });
