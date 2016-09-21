1b9752999ae9e3f6c514bf94a7bc6e4ff76bfd83
Slobodan Djordjevic <sdjordjevic@razor.rs>
2016-09-21 06:41:28 +0200

    Main controller and test

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,16 +2,23 @@
   'use strict';
 
   /**
-     * @ngdoc controller
-     * @name tamedia.controller:MainController
-     */
+   * @ngdoc service
+   * @name tamedia.controller:MainController
+   *
+   * @description
+   * Home view controller
+   *
+   * @requires productList
+   */
 
   angular
     .module('tamedia')
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController(productList) {
+    var vm = this;
 
+    vm.productList = productList.data.articles;
   }
 })();
