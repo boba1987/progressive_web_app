e3669b45d89560cee3142cb338574f9c9e8332cb
Slobodan Djordjevic <sdjordjevic@razor.rs>
2016-09-21 06:39:43 +0200

    main controller e2e and unit test

--- e2e/main.po.js
+++ e2e/main.po.js
@@ -6,10 +6,8 @@
 'use strict';
 
 var MainPage = function() {
-  this.jumbEl = element(by.css('.jumbotron'));
-  this.h1El = this.jumbEl.element(by.css('h1'));
-  this.imgEl = this.jumbEl.element(by.css('img'));
-  this.thumbnailEls = element(by.css('body')).all(by.repeater('awesomeThing in main.awesomeThings'));
+  this.articlesListEl = element(by.css('.container-list'));
+  this.articlesListEls = element(by.css('.container-list')).all(by.repeater('item in main.productList'));
 };
 
 module.exports = new MainPage();
